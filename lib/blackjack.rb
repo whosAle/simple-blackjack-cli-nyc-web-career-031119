535fe8ca2ae989de0a11716e4e75b7b023243acc
A Belgrave <abelgrave95@gmail.com>
2019-02-24 19:46:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -27,8 +27,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card + deal_card
-#  display_card_total(deal_card + deal_card)
+  sum = deal_card + deal_card
+  display_card_total(sum)
 end
 
 def hit?(current_sum)
