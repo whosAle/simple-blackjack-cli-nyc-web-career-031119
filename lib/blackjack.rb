aabeaf2acc8aeec51d09599776754da36abd645a
A Belgrave <abelgrave95@gmail.com>
2019-02-24 20:17:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,7 +63,7 @@ def runner
   welcome
 
   card_total = initial_round
-  while card_total < 21
+  while card_total <= 21
     puts "akljwfwklfjalk #{card_total}"
     card_total = hit?(card_total)
   end
